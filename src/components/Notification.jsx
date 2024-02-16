a7230ee95666aa8e0707477705cbccbf60389074
Juan Cruz Caceres <cacerescallieri@gmail.com>
2024-02-16 17:19:20 -0300

    Fix html stucture to accommodate better resemblance with design

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,17 +2,21 @@ const Notification = ({isNew, profileName, notificationAction, relevantLink, tim
     const srcProfileName = profileName.toLowerCase().replace(/ /g, '-')
 
     return (
-    <div className={`notification-container ${isNew && `new-notification-container`}`}>
-        <img src={`avatar-${srcProfileName}.webp`} alt="avatar" className="avatar"/>
-        <div className="notification">
-            <div className="notification-text">
-                <div className=""><a className='profile-name'>{profileName}</a>&nbsp; {notificationAction} <a className={`relevant-link ${relevantLink.isGroup && "group"}`}>&nbsp; {relevantLink && relevantLink.message}</a><div className={`${isNew && `new-notification-icon`}`}></div></div>
-                {imageUrl && <img src={imageUrl} className="notification-image" />}
+        <>
+            <div className={`notification-container ${isNew && `new-notification-container`}`}>
+                <img src={`avatar-${srcProfileName}.webp`} alt="avatar" className="avatar"/>
+                <div className="private-message-container">
+                    <div className="notification">
+                        <div className="notification-body">
+                            <div className="notification-text"><a className='profile-name'>{profileName}</a>&nbsp; {notificationAction} <a className={`relevant-link ${relevantLink.isGroup && "group"}`}>&nbsp; {relevantLink && relevantLink.message}</a><div className={`${isNew && `new-notification-icon`}`}></div></div>
+                            <p className='time'>{time} ago</p>
+                        </div>
+                        {imageUrl && <img src={imageUrl} className="notification-image" />}
+                    </div>
+                    {privateMessage && <p className='private-message'>{privateMessage}</p>}
+                </div>
             </div>
-            <p className='time'>{time} ago</p>
-            {privateMessage && <p className='private-message'>{privateMessage}</p>}
-        </div>
-    </div>
+        </>
     )
 }
 
